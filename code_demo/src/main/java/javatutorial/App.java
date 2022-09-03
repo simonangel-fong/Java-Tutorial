4a3da96c0ccdd229c411f226fc77660136778b64
Simon Fong <simonangelfong@gmail.com>
2022-09-03 11:47:24 +0800

    create maven project files

---
+++ code_demo/src/main/java/javatutorial/App.java
@@ -0,0 +1,13 @@
+package javatutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
